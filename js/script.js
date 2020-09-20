44838ce1a2c5674c1b916b2ab9f0506b4b6410da
Ilez Kurskiev <ilez.kurskiev@gmail.com>
2020-09-21 00:26:45 +0300

    some fixes

--- js/script.js
+++ js/script.js
@@ -23,118 +23,16 @@ function setCoefficient () {
 
 setCoefficient()
 
-function detectMouseWheelDirection( event ) {
-    var delta = null
-    var direction = false
-
-    if (!event) {
-      event = window.event;
-    }
-
-    if (event.wheelDelta) {
-      delta = event.wheelDelta / 60;
-    } else if (event.detail) {
-      delta = -event.detail / 2;
-    }
-
-    if (delta !== null) {
-      direction = delta > 0 ? 'up' : 'down';
-    }
-
-    return direction;
-}
-
-var slideTimeout
-var delay = 1001
-var sectionElement = document.getElementById("section4")
-var fullpageElement = document.getElementById("fullpage")
-var wrapper = document.querySelector(".wrapper")
-var isFirefox = window.navigator.userAgent.toLowerCase().indexOf('firefox') > -1
-
 if (window.innerWidth > 990) {
-	if (!isFirefox) {
-		sectionElement.classList.add("absolute", "top")
-	} else {
-		fullpageElement.appendChild(sectionElement)
-	}
-
 	var instance = new fullpage("#fullpage", {
 		licenseKey: "B6BC1205-0D4C4A40-B4ECE2E7-85523C97",
 		easingcss3: "cubic-bezier(0.76, 0, 0.24, 1)",
-		scrollingSpeed: delay,
-		onLeave: function (section, destination, direction) {
-			if (!destination.isLast) {
-				setLock(false, destination.index)
-
-				if (direction === "down") {
-					section.item.classList.add("translate")
-				} else {
-					destination.item.classList.remove("translate")
-				}
-			} else {
-				setLock(true, destination.index)
-			}
+		scrollingSpeed: 1000,
+		absoluteSlide: {
+			element: document.getElementById("section4"),
+			position: 4
 		}
 	})
-
-	wrapper.onmousewheel = mouseWheelHandle;
-	wrapper.addEventListener('DOMMouseScroll', mouseWheelHandle);
-}
-
-function setLock (isLock, slide) {
-	clearTimeout(slideTimeout)
-	slideTimeout = setTimeout(function () {
-		window.isLock = isLock
-
-		if (slide) {
-			window.slide = slide
-		}
-
-		slideTimeout = null
-	}, delay)
-}
-
-function mouseWheelHandle (event) {
-	if (isFirefox) {
-		return
-	}
-
-	var direction = detectMouseWheelDirection(event)
-	var isFullPage = event.currentTarget.contains(sectionElement)
-
-	if (window.slide === 3 && direction === "down") {
-		event.stopPropagation()
-		sectionElement.classList.remove("top")
-		sectionElement.classList.add("show")
-
-		if (!slideTimeout) {
-			setLock(true)
-		}
-	}
-
-	if (window.isLock) {
-		event.stopPropagation()
-
-		if (direction === "down") {
-			sectionElement.classList.remove("top")
-			sectionElement.classList.add("show")
-			if (!slideTimeout) {
-				setLock(true)
-			}
-		} else {
-			sectionElement.classList.remove("show")
-			if (!slideTimeout) {
-				setLock(false)
-			}
-		}
-	}
-
-	if (isFullPage && direction === "up") {
-		sectionElement.classList.remove("show")
-		if (!slideTimeout) {
-			setLock(false)
-		}
-	}
 }
 
 document.querySelector("#email").onsubmit = function (event) {
